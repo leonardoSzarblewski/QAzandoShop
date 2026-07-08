54af7600ab932081953af79f9a346cba7758a12e
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-08 20:52:47 -0300

    test: add logout functionality tests

---
+++ cypress/e2e/logout.cy.ts
@@ -0,0 +1,34 @@
+import { userData } from '../support/factories/user'
+
+describe('login', () => {
+  
+  beforeEach(() => {
+    cy.visit('/login')
+
+    cy.fillsLogin(userData.email, userData.password)
+    cy.get('#btnLogin').click()
+
+    cy.contains('button', 'OK').click()
+  })
+
+  it('Should log out the user', () => {
+    cy.contains('a', 'logout').click()
+
+    cy.get('#swal2-title').should('contain', 'Logout Sucessfull')
+    cy.contains('button', 'OK').click()
+    
+    cy.url().should('eq', 'https://automationpratice.com.br/login')
+  })
+
+  it('Should log out through the user menu', () => {
+    cy.contains('#userLogged', userData.email)
+        .find('a[href="/my-account#!"]')
+        // Não é recomendado utilizar force: true, mas é necessário neste caso devido a um bug na interface que impede o clique no elemento.
+        .click({ force: true }) 
+
+    cy.get('#swal2-title').should('contain', 'Logout realizado')
+    cy.contains('button', 'OK').click()
+
+    cy.url().should('eq', 'https://automationpratice.com.br/login#!')
+  })
+})
