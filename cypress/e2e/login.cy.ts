acf70f9247b1ac8e5b5465f05b37188eda57dd3d
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-08 19:33:22 -0300

    feat: implement user data factory for login tests

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,11 +1,5 @@
-import { faker } from '@faker-js/faker'
+import { userData } from '../support/factories/user'
 
-const user = {
-  email: faker.internet.email(),
-  password: faker.internet.password({ length: 6 }),
-  invalidEmail: 'teste',
-  invalidPassword: faker.internet.password({ length: 5 }),
-}
 
 describe('login', () => {
   
@@ -14,18 +8,18 @@ describe('login', () => {
   })
 
   it('Should log in successfully', () => {
-    cy.fillsLogin(user.email, user.password)
+    cy.fillsLogin(userData.email, userData.password)
     cy.get('#btnLogin').click()
 
     cy.get('#swal2-title').should('contain', 'Login realizado')  
-    cy.get('#swal2-html-container').should('contain', 'Olá, ' + user.email)
+    cy.get('#swal2-html-container').should('contain', 'Olá, ' + userData.email)
 
     cy.contains('button', 'OK').click()
-    cy.get('#userLogged').should('contain', user.email)
+    cy.get('#userLogged').should('contain', userData.email)
   })
 
   it('Should indicate that the email field is required', () => {
-    cy.fillsLogin(user.email, user.password)
+    cy.fillsLogin(userData.email, userData.password)
     cy.get('#user').clear()
     cy.get('#btnLogin').click()
 
@@ -33,7 +27,7 @@ describe('login', () => {
   })  
 
   it('Should indicate that the password field is required', () => {
-    cy.fillsLogin(user.email, user.password)
+    cy.fillsLogin(userData.email, userData.password)
     cy.get('#password').clear()
     cy.get('#btnLogin').click()
 
@@ -41,14 +35,14 @@ describe('login', () => {
   })  
 
   it('Should show an error when the email format is invalid', () => {
-    cy.fillsLogin(user.invalidEmail, user.password)
+    cy.fillsLogin(userData.invalidEmail, userData.password)
     cy.get('#btnLogin').click()
 
     cy.contains('span', 'E-mail inválido.').should('be.visible')
   })  
 
   it('Should show an error when the password has fewer than 6 characters', () => {
-    cy.fillsLogin(user.email, user.invalidPassword)
+    cy.fillsLogin(userData.email, userData.invalidPassword)
     cy.get('#btnLogin').click()
 
     cy.contains('span', 'Senha inválida.').should('be.visible')
---
+++ cypress/support/factories/user.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+
+export interface userData {
+  name: string
+  email: string
+  password: string
+  invalidEmail: string
+  invalidPassword: string
+}
+
+export const userData: userData = {
+  name: faker.person.firstName(),
+  email: faker.internet.email(),
+  password: faker.internet.password({ length: 6 }),
+  invalidEmail: 'teste',
+  invalidPassword: faker.internet.password({ length: 5 }),
+}
+
