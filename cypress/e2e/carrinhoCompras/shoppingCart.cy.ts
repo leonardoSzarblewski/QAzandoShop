1ab0e95a92084dd603f901f3d0cf7414a0434b5b
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-10 19:51:51 -0300

    feat: add shopping cart functionality tests

---
+++ cypress/e2e/carrinhoCompras/shoppingCart.cy.ts
@@ -0,0 +1,51 @@
+import { userData } from '../../support/factories/user'
+
+describe('Shopping Cart', () => {
+  
+  beforeEach(() => {
+    cy.visit('/login')
+
+    cy.fillsLogin(userData.email, userData.password)
+    cy.get('#btnLogin').click()
+
+    cy.contains('button', 'OK').click()
+  })
+
+  it('should add an item to the shopping cart', () => {
+    cy.get(':nth-child(3) > .offcanvas-toggle > .fa').click()
+    cy.contains('a', 'Maxi Dress').should('not.exist')
+    cy.get('#offcanvas-add-cart > .offcanvas-header > .offcanvas-close').click()
+
+    cy.openShop()
+
+    cy.get(':nth-child(4) > .product_wrappers_one > .thumb > .image > .hover-image').click()
+    cy.contains('a', 'Add To Cart').click()
+
+    cy.get('#swal2-html-container').should('contain', 'Successfully added to your Cart')
+
+    cy.get(':nth-child(3) > .offcanvas-toggle > .fa').click()
+    cy.contains('a', 'Maxi Dress').should('exist')
+  })
+
+  it('should display an error message when trying to add an item that is already in the cart', () => {
+    cy.openShop()
+
+    cy.get(':nth-child(5) > .product_wrappers_one > .thumb > .image > .hover-image').click()
+    cy.contains('a', 'Add To Cart').click()    
+
+    cy.get('#swal2-html-container').should('contain', 'This product is already added in your Cart')
+  })
+
+  it('should change the price when deleting an item from the cart', () => {
+    cy.get(':nth-child(3) > .offcanvas-toggle > .fa').click()
+    cy.contains('a', 'Fit-Flare Dress').should('exist')
+    cy.contains('span', '$107.00').should('be.visible')
+    cy.get('.offcanvas-cart > :nth-child(1) > .text-right > .offcanvas-wishlist-item-delete > .fa').click()
+
+    cy.contains('span', '$55.00').should('be.visible')
+    cy.contains('a', 'Fit-Flare Dress').should('not.exist')
+    
+  })
+
+
+})
