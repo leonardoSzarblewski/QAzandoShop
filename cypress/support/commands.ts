989cee08283485b99c9385f09f0464868c76dca3
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-13 12:11:08 -0300

    feat: add openDetailsCart command to access cart details

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -19,3 +19,8 @@ Cypress.Commands.add('openShop', () => {
     cy.get(':nth-child(2) > :nth-child(1) > .mobile-sub-menu > :nth-child(1) > a').click();
 });
 
+// Deve acessar os detalhes do carrinho de compras
+Cypress.Commands.add('openDetailsCart', () => {
+    cy.get(':nth-child(3) > .offcanvas-toggle > .fa').click();
+    cy.contains('a', 'View Cart').click();
+});
