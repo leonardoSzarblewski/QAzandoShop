2e420a49c2ece7d404e9c6cc8237d64e1a9c8370
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-14 15:30:59 -0300

    chore: update Node.js version to 22 in CI workflow and clean up Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,18 @@
-const { defineConfig } = require("cypress");
-const { startDevServer } = require("@cypress/webpack-dev-server");
-const webpackConfig = require("./webpack.config");
+const { defineConfig } = require('cypress');
+const { startDevServer } = require('@cypress/webpack-dev-server');
+const webpackConfig = require('./webpack.config');
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  video: true,
 
   e2e: {
-    baseUrl: "https://automationpratice.com.br",
-    specPattern: "cypress/e2e/**/*.cy.ts",
-    supportFile: "cypress/support/e2e.ts",
+    baseUrl: 'https://automationpratice.com.br',
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+    supportFile: 'cypress/support/e2e.ts',
 
     setupNodeEvents(on, config) {
-      on("dev-server:start", (options) => startDevServer({ options, webpackConfig }));
+      on('dev-server:start', (options) => startDevServer({ options, webpackConfig }));
       return config;
     },
   },
