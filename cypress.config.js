0a84e9614953f92654cfb21e2c20f52119f2107b
leonardoszarblewski <leonardo27.laurindo@gmail.com>
2026-07-17 13:09:47 -0300

    refactor: remove unused webpack configuration and related dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('cypress');
-const { startDevServer } = require('@cypress/webpack-dev-server');
-const webpackConfig = require('./webpack.config');
 
 module.exports = defineConfig({
   allowCypressEnv: false,
@@ -12,7 +10,6 @@ module.exports = defineConfig({
     supportFile: 'cypress/support/e2e.ts',
 
     setupNodeEvents(on, config) {
-      on('dev-server:start', (options) => startDevServer({ options, webpackConfig }));
       return config;
     },
   },
